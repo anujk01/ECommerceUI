1ced4b1ead14e8fd228914c50dd6673a6c6b8252
AnujKumar <anuj.kumarr.97@gmail.com>
2026-05-15 00:20:12 +0530

    code update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // filename must match exactly
+import App from "./App.js"; // Explicit extension helps with case sensitivity in Linux environments
 import "./styles/login.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
